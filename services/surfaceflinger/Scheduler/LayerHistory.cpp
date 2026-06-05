42df00bb7973864e657393e3481989c8c71e2b92
John Galt <johngaltfirstrun@gmail.com>
2026-06-05 12:52:10 -0400

    surfaceflinger: specifically no vote status bar
    Stops clock updates, battery or signal changes, or network monitor (if
    configured to be shown in status bar) from voting at all.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/surfaceflinger/Scheduler/LayerHistory.cpp
+++ services/surfaceflinger/Scheduler/LayerHistory.cpp
@@ -355,6 +355,10 @@ void LayerHistory::partitionLayers(nsecs_t now) {
                 info->resetLayerVote();
             }
 
+            if (layerUnsafe->getWindowType() == gui::WindowInfo::Type::STATUS_BAR) {
+                info->setLayerVote({LayerVoteType::NoVote, Fps()});
+            }
+
             it++;
         } else {
             if (CC_UNLIKELY(mTraceEnabled)) {
